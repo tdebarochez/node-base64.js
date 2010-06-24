ad1911db451f18f10cc9c27d2c6dcf6867ddf0a6
Thomas Debarochez <thomas.barochez+github@gmail.com>
2010-06-25 00:52:26 +0200

    add some tests

--- test.js
+++ test.js
@@ -4,3 +4,9 @@ var sys = require('sys'),
 
 sys.puts(sys.inspect(base64.encode(base64.decode(fs.readFileSync('test.txt', 'ascii'))) == fs.readFileSync('test.txt', 'ascii').replace(/([\r\n]+)/gi, '')));
 sys.puts(sys.inspect(base64.decode(base64.encode("ABCD")) == "ABCD"));
+sys.puts(sys.inspect(base64.encode('What do ya want for nothing?')=='V2hhdCBkbyB5YSB3YW50IGZvciBub3RoaW5nPw=='));
+sys.puts(sys.inspect(base64.decode(base64.encode('What do ya want for nothing?'))=='What do ya want for nothing?'));
+var textBuff = 'What do ya want for nothing?';
+var baseBuff = 'V2hhdCBkbyB5YSB3YW50IGZvciBub3RoaW5nPw==';
+sys.puts(sys.inspect(base64.encode(textBuff)=='V2hhdCBkbyB5YSB3YW50IGZvciBub3RoaW5nPw=='));
+sys.puts(sys.inspect(base64.decode(baseBuff)=='What do ya want for nothing?'));
